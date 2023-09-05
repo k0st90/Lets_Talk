00ae1203e634532fa5fe950cf8ce4881d16fdba4
Konstantin <hghghge8@gmail.com>
2023-09-05 23:44:49 +0300

    registration template

--- letstalk/base/views.py
+++ letstalk/base/views.py
@@ -2,15 +2,28 @@ from django.shortcuts import render, redirect
 from django.db.models import Q
 from .models import Room, Topic
 from .forms import RoomForm
+from django.contrib.auth.models import User
 
 # Create your views here.
+
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            
+    context = {}
+    return render(request, 'base/login_register.html', context)
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
     rooms = Room.objects.filter(
         Q(topic__name__icontains=q) |
         Q(name__icontains=q) |
-        Q(description__icontains=q) 
+        Q(description__icontains=q)
         )
     room_count = rooms.count()
     topics = Topic.objects.all()
