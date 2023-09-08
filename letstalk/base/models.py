79031e0973959f1ad206021b67265471dfd07aac
Konstantin <hghghge8@gmail.com>
2023-09-08 13:40:37 +0300

    crud for messages

--- letstalk/base/models.py
+++ letstalk/base/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-
 # Create your models here.
 
 class Topic(models.Model):
@@ -14,9 +13,9 @@ class Room(models.Model):
     topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
-    #participants = 
-    updated = models.DateField(auto_now=True)
-    created = models.DateField(auto_now_add=True)
+    participants = models.ManyToManyField(User, related_name='participants', blank=True)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         ordering = ['-updated', '-created']
@@ -28,8 +27,8 @@ class Message(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
     body = models.TextField()
-    updated = models.DateField(auto_now=True)
-    created = models.DateField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self) -> str:
         return self.body[0:50]
