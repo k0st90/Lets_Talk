f93fb5977e0efe79367440002f79ba38c1c94ea8
Konstantin <hghghge8@gmail.com>
2023-09-28 23:57:25 +0300

    Mobile Responsiveness

--- letstalk/base/views.py
+++ letstalk/base/views.py
@@ -66,7 +66,7 @@ def home(request):
         )
     
     room_count = rooms.count()
-    topics = Topic.objects.all()
+    topics = Topic.objects.all()[0:5]
     room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
 
     context = {'rooms': rooms, 'topics':topics, 
@@ -175,3 +175,12 @@ def updateUser(request):
             return redirect('user-profile', pk=user.id)
 
     return render(request, 'base/update-user.html', {'form':form})
+
+def topicsPage(request):
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    topics = Topic.objects.filter(name__icontains=q)
+    return render(request, 'base/topics.html', {'topics':topics})
+
+def activityPage(request):
+    room_messages = Message.objects.all()
+    return render(request, 'base/activity.html', {'room_messages':room_messages})
